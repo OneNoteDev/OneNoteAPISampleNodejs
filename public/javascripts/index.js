e48ef0a132aee11b83f126cc8e2a405dcfac6a77
Juan Corona <juanc@evidentpoint.com>
2014-04-28 12:52:42 -0700

    Add a spinner

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,6 +5,12 @@ $(function () {
     $loginBtn = $('#loginBtn');
     updateLoginButton(false);
     checkLogin();
+    $('#createExamples').find('button').each(function(){
+        $(this).on('mouseup',function(e){
+            $(e.target).after('<img src="images/spinner.gif" style="margin-left: 5px;"/>');
+            window.setTimeout(function(){disableCreateButtons(true);},0);
+        });
+    });
 });
 
 function openPopUp(url) {
